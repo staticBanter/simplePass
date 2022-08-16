4c07a6d9e1c310ca41c883d7d2536a9151d09e59
staticBanter <staticbanter@staticblogs.ca>
2022-08-16 01:32:35 -0400

    feat: add tests to cover a wider variety of options.
    Signed-off-by: staticBanter <staticbanter@staticblogs.ca>

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,168 @@
 import simplePass from "../simplePass/simplePass.js";
 const assert = chai.assert;
 
-describe('simplePass | Genereate Lowercase Passwords',function(){
+describe('simplePass | Genereate A Lowercase Password Of Defualt Length',function(){
+
+    const password = simplePass();
 
     it('should create a password containing only lowercase characters', function(){
-        assert.match(simplePass(),/[a-z]/);
+        assert.match(password,/[a-z]/g);
     });
 
     it('should create a password of default length (8)', function(){
-        assert.lengthOf(simplePass(),8);
+        assert.lengthOf(password,8);
     });
 
     it('should create a password NOT containing any uppercase characters', function(){
-        assert.notMatch(simplePass(),/[A-Z]/);
+        assert.notMatch(password,/[A-Z]/g);
     });
 
     it('should create a password NOT containing any numbers', function(){
-        assert.notMatch(simplePass(),/[0-9]/);
+        assert.notMatch(password,/[0-9]/g);
+    });
+
+    it('should create a password NOT containing any puncutation',function(){
+        assert.notMatch(password,/[^A-Za-z0-9]/g)
+    });
+
+});
+
+describe('simplePass | Genereate An Uppercase Password Of Defualt Length',function(){
+
+    const password = simplePass({
+        length:8,
+        lowercase:false,
+        uppercase:true,
+        numbers:false,
+        punctuation:false,
+        special:false,
+        memorable:false
+    });
+
+    it('should create a password containing only uppercase characters', function(){
+        assert.match(password,/[A-Z]/g);
+    });
+
+    it('should create a password of default length (8)', function(){
+        assert.lengthOf(password,8);
+    });
+
+    it('should create a password NOT containing any lowercase characters', function(){
+        assert.notMatch(password,/[a-z]/g);
+    });
+
+    it('should create a password NOT containing any numbers', function(){
+        assert.notMatch(password,/[0-9]/g);
+    });
+
+    it('should create a password NOT containing any puncutation',function(){
+        assert.notMatch(password,/[^A-Za-z0-9]/g)
+    });
+
+});
+
+describe('simplePass | Genereate A Number Password Of Defualt Length',function(){
+
+    const password = simplePass({
+        length:8,
+        lowercase:false,
+        uppercase:false,
+        numbers:true,
+        punctuation:false,
+        special:false,
+        memorable:false
+    });
+
+    it('should create a password containing only numbers', function(){
+        assert.match(password,/[0-9]/g);
+    });
+
+    it('should create a password of default length (8)', function(){
+        assert.lengthOf(password,8);
+    });
+
+    it('should create a password NOT containing any lowercase characters', function(){
+        assert.notMatch(password,/[a-z]/g);
+    });
+
+    it('should create a password NOT containing any uppercase characters', function(){
+        assert.notMatch(password,/[A-Z]/g);
+    });
+
+    it('should create a password NOT containing any puncutation',function(){
+        assert.notMatch(password,/[^A-Za-z0-9]/g)
+    });
+
+});
+
+describe('simplePass | Genereate A Punctuation Password Of Defualt Length',function(){
+
+    const password = simplePass({
+        length:8,
+        lowercase:false,
+        uppercase:false,
+        numbers:false,
+        punctuation:true,
+        special:false,
+        memorable:false
+    });
+
+    it('should create a password containing only punctuation', function(){
+        assert.match(password,/[^A-Za-z0-9]/g);
+    });
+
+    it('should create a password of default length (8)', function(){
+        assert.lengthOf(password,8);
+    });
+
+    it('should create a password NOT containing any lowercase characters', function(){
+        assert.notMatch(password,/[a-z]/g);
+    });
+
+    it('should create a password NOT containing any uppercase characters', function(){
+        assert.notMatch(password,/[A-Z]/g);
+    });
+
+    it('should create a password NOT containing any numbers',function(){
+        assert.notMatch(password,/[0-9]/g)
+    });
+
+});
+
+
+/**
+ * * Note: sometimes this functions fails because there is not conformance check on the passwords
+ */
+describe('simplePass | Genereate A Random Password Of Defualt Length',function(){
+
+    const password = simplePass({
+        length:8,
+        lowercase:true,
+        uppercase:true,
+        numbers:true,
+        punctuation:true,
+        special:false,
+        memorable:false
+    });
+
+    it('should create a password containing punctuation', function(){
+        assert.match(password,/[^A-Za-z0-9]/g);
+    });
+
+    it('should create a password of default length (8)', function(){
+        assert.lengthOf(password,8);
+    });
+
+    it('should create a password containing lowercase characters', function(){
+        assert.match(password,/[a-z]/g);
+    });
+
+    it('should create a password containing uppercase characters', function(){
+        assert.match(password,/[A-Z]/g);
+    });
+
+    it('should create a password containing numbers',function(){
+        assert.match(password,/[0-9]/g)
     });
 
 });
