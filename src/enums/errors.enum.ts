2ffc60de8b8885c53b54f402bc594cc690526a80
staticBanter <staticbanter@staticblogs.ca>
2022-09-23 21:12:11 -0400

    fix: incorrect error code formatting.
    There was a lowercase p when it should have been uppercase :$
    
    Signed-off-by: staticBanter <staticbanter@staticblogs.ca>

--- src/enums/errors.enum.ts
+++ src/enums/errors.enum.ts
@@ -10,7 +10,7 @@
 enum E_errors{
     invalidLength = 'sP_E.1: The given length was not valid. A valid input length must be a number between 1 and 256.',
     invalidModifier = 'sP_E.2: The given modifier was not valid',
-    invalidWhitespaceOption = 'sp_E.3: The given whitespace options were not valid.',
+    invalidWhitespaceOption = 'sP_E.3: The given whitespace options were not valid.',
     invalidCharCheckRestriction = 'sP-cC_E.1: The restriction used to check a character was not valid.',
     invalidNumberOfSelectedModifiers = 'sP-cP_E.1: The password was not long enough to contain the selected number of modifiers.',
 }
